2624e5fc5d7fa94f28ac84504b0cc905cff379d5
hgy4152 <hgy4152@gmail.com>
2026-04-03 00:10:21 +0900

    SurvivalShooter
    fsadf

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public TextMeshPro scoreText;
+
+    public Slider effectVolume;
+    public Slider musicVolume;
+
+    public Button quitGame;
+    public Button Resume;
+
+    public Toggle sound;
+
+
+}
